42d9c3cde099e4e68b57362577e0ced48d3b0960
Gen Ye <inuyasha.yegen@gmail.com>
2019-04-08 04:36:53 +0800

    test

---
+++ src/main/java/com/xboxinuyasha/Application.java
@@ -0,0 +1,18 @@
+package com.xboxinuyasha;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class)
+                .bannerMode(Banner.Mode.OFF)
+                .properties(new String[]{
+                        "spring.mvc.favicon.enabled=false"
+                })
+                .run(args);
+    }
+
+}
---
+++ src/main/java/com/xboxinuyasha/rest/ExampleApi.java
@@ -0,0 +1,23 @@
+package com.xboxinuyasha.rest;
+
+import com.alibaba.fastjson.JSONObject;
+import com.xboxinuyasha.bean.JsonResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("exampleApi")
+@RequestMapping("/api/example")
+public class ExampleApi {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/user/test")
+    public JsonResult<JSONObject> selectUserTest() {
+        JSONObject user = new JSONObject();
+        user.put("username", "yegen");
+        user.put("password", "521100");
+        if (user.isEmpty()) {
+            return JsonResult.error(0, "网络错误");
+        }
+        return JsonResult.success(user);
+    }
+}
